e661c21078b81630b2cad50bd2353de7d17a9e09
STYJ <simon.tan.yu.jing@gmail.com>
2020-06-30 15:53:58 +0800

    updated zil context

--- src/contexts/zil-context/index.tsx
+++ src/contexts/zil-context/index.tsx
@@ -16,21 +16,7 @@
  */
 
 import React from 'react';
-import {
-  getAddressFromPrivateKey,
-  getPubKeyFromPrivateKey,
-  fromBech32Address
-} from '@zilliqa-js/crypto';
-import { Long, bytes, units, BN } from '@zilliqa-js/util';
-import { Transaction } from '@zilliqa-js/account';
-
-import { Zilliqa } from '@zilliqa-js/zilliqa';
-import { HTTPProvider, RPCMethod } from '@zilliqa-js/core';
-import { NODE_URL, CHAIN_ID, MSG_VERSION } from '../../constants';
-
-const provider = new HTTPProvider(NODE_URL);
-const zilliqa = new Zilliqa(NODE_URL, provider);
-const version = bytes.pack(CHAIN_ID, MSG_VERSION);
+import { fromBech32Address } from '@zilliqa-js/crypto';
 
 const getHost = (host: string) => {
   switch (host) {
@@ -38,81 +24,13 @@ const getHost = (host: string) => {
       return 'https://nucleus-server.zilliqa.com';
   }
 };
-
-const isOk = (code) => code < 400;
-
 const initialState: any = {
-  wallet: zilliqa.wallet,
-  isAuth: undefined,
-  address: undefined,
-  publicKey: undefined,
-  privateKey: undefined
 };
 
 export const ZilContext = React.createContext(initialState);
 
 export class ZilProvider extends React.Component {
   public readonly state = initialState;
-
-  public accessWallet = (privateKey: string) => {
-    try {
-      const address = getAddressFromPrivateKey(privateKey);
-      const publicKey = getPubKeyFromPrivateKey(privateKey);
-      const { wallet } = this.state;
-      wallet.addByPrivateKey(privateKey);
-
-      this.setState({
-        isAuth: true,
-        privateKey,
-        address,
-        publicKey,
-        wallet
-      });
-    } catch (error) {
-      this.setState({ isAuth: false });
-    }
-  };
-
-  private getParams = async (toAddr, amountInZil) => {
-    const response = await zilliqa.blockchain.getMinimumGasPrice();
-    const gasPrice: string = response.result || '';
-
-    const amountInQa = units.toQa(amountInZil, units.Units.Zil);
-    return {
-      toAddr,
-      version,
-      amount: amountInQa,
-      gasPrice: new BN(gasPrice.toString()),
-      gasLimit: Long.fromNumber(1)
-    };
-  };
-
-  public send = async ({ args }): Promise<any> => {
-    const { amount, toAddress } = args;
-    const { wallet } = this.state;
-    const tx = new Transaction(await this.getParams(toAddress, amount), provider);
-    const signedTx = await wallet.sign(tx);
-    const { txParams } = signedTx;
-    // Send a transaction to the network
-    const res = await provider.send(RPCMethod.CreateTransaction, txParams);
-    if (res.error !== undefined) throw new Error(res.error.message);
-    return res.result ? res.result.TranID : undefined;
-  };
-
-  public getBalance = async (): Promise<string> => {
-    const { address } = this.state;
-    const res = await zilliqa.blockchain.getBalance(address);
-    // if (res.error !== undefined) throw new Error(res.error.message);
-    if (res.error !== undefined) return '0';
-    return res.result ? res.result.balance : '0';
-  };
-
-  public getMinGasPrice = async (): Promise<string> => {
-    const res = await zilliqa.blockchain.getMinimumGasPrice();
-    if (res.error !== undefined) throw new Error(res.error.message);
-    return res.result ? res.result : '0';
-  };
-
   public faucet = async ({ args, signal }): Promise<string | void> => {
     const { token, toAddress } = args;
     const address = fromBech32Address(toAddress);
@@ -130,20 +48,10 @@ export class ZilProvider extends React.Component {
     return data ? data.txId : undefined;
   };
 
-  public clearAuth = () => {
-    this.setState({ ...initialState });
-  };
-
   public render() {
     return (
       <ZilContext.Provider
         value={{
-          ...this.state,
-          clearAuth: this.clearAuth,
-          accessWallet: this.accessWallet,
-          getBalance: this.getBalance,
-          getMinGasPrice: this.getMinGasPrice,
-          send: this.send,
           faucet: this.faucet
         }}
       >
